2faff90b98681007ce84b6cc0a949fdcbc094059
DHolets99 <dholets99@gmail.com>
2022-08-22 17:23:55 +0300

    22.08 17.23

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
+const slider = document.querySelectorAll('.slider_wrapper');
+const next = document.querySelector('.btn_right');
+const prev = document.querySelector('.btn_left');
 const slides = [];
 
 let slidesToShow = 3;
 let slidesToScroll = 1;
 let currentSlide = 0;
+
+const check = () => {
+    console.log (1);
+}
+
+
+next.addEventListener('click', check);
