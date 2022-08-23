d80c8b06ea0738667763c567128b0035c699c017
DHolets99 <dholets99@gmail.com>
2022-08-23 16:44:23 +0300

    23.08 16.44

---
+++ js/slider.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        centerMode: true,
+        arrows: true,
+        centerPadding: '30px',
+        slidesToShow: 3,
+        responsive: [
+            {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '30px',
+                slidesToShow: 3
+            }
+            },
+            {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+            }
+        ]
+    });
+});
